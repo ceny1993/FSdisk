a02a24cb693b8a5df0bf1b443c0fa6355deed133
ceny1993 <ceny1993@foxmail.com>
2017-03-21 23:21:27 +0800

    fix

--- src/main/java/com/ceny/config/database/CustomerRepo.java
+++ src/main/java/com/ceny/config/database/CustomerRepo.java
@@ -1,13 +1,13 @@
 package com.ceny.config.database;
 
 import com.ceny.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by chensongkui on 2017/3/21.
  */
 @Repository
-public interface CustomerRepo extends CrudRepository<Customer,Long> {
+public interface CustomerRepo extends JpaRepository<Customer,Long> {
 }
